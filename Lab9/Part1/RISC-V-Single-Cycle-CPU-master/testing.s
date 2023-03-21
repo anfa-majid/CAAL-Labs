34c2c12bd0447e0a62cc9bafffc8710e2952feb3
Anfa Majid <110888247+anfa-majid@users.noreply.github.com>
2023-03-21 12:48:31 +0500

    Initial commit

---
+++ Lab9/Part1/RISC-V-Single-Cycle-CPU-master/testing.s
@@ -0,0 +1,45 @@
+
+        addi t0,x0,65 #A
+        sw t0, 0(s1)
+        
+        addi t0,x0,110 #n
+        sw t0, 4(s1)
+        
+        addi t0,x0,102 #f
+        sw t0, 8(s1)
+        
+        addi t0,x0, 97 #a
+        sw t0,12(s1)
+        
+        addi t0,x0 77 #M
+        sw t0,16(s1)
+        
+        addi t0,x0, 97#a
+        sw t0, 20(s1)
+        
+        addi t0,x0,106 #j
+        sw t0, 24(s1)
+        
+        addi t0,x0, 105 #i
+        sw t0, 28(s1)
+        
+        addi t0,x0,100 #d
+        sw t0,32(s1)
+        
+        addi t0,x0, 32 #space
+        sw t0, 36(s1)
+        
+        addi t0,x0,50 #2
+        sw t0,40(s1)
+        
+        addi t0,x0, 53 #5
+        sw t0, 44(s1)
+        
+        addi t0,x0 , 49 #1
+        sw t0,48(s1)
+        
+        addi t0,x0, 53 #5
+        sw t0, 52(s1)
+        
+        addi t0,x0, 53 #5
+        sw t0, 56(s1)
